a737a9c1ad847229882788ae358ddec66e760380
Tatiane Pimentel <tatiane.toth@gmail.com>
2023-05-04 16:53:58 -0400

    setup project

---
+++ challenge-event/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge-event
+//
+//  Created by Tatiane Toth on 04/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
